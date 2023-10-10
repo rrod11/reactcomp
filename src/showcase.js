d482e3bb4fe2aac1274a71779431808e4227745d
Roderick <Roderickj800@gmail.com>
2023-10-10 12:15:52 -0400

    css not working

--- src/showcase.js
+++ src/showcase.js
@@ -1,7 +1,18 @@
+import Char from "./images/charizard.jpg";
 function Showcase() {
+  const favPokemon = "Charizard";
+  const pokeCharacteristics = {
+    type: "fire",
+    move: "Incinerate",
+  };
   return (
     <div>
-      <h1>Showcase Component</h1>
+      {favPokemon}s<h1>Showcase Component</h1>
+      <img src={Char} alt="Charizard"></img>
+      <h2>
+        Charizard's type if <span>{pokeCharacteristics.type}</span> and one of
+        their moves is <span>{pokeCharacteristics.move}<span/>
+      </h2>
     </div>
   );
 }
